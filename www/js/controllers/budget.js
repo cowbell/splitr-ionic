ed2f2f0324f12db0812cc96973212893580bbadc
Tomasz Subik <tsubik@gmail.com>
2014-10-16 20:57:36 +0200

    quick member adding, total for member

--- www/js/controllers/budget.js
+++ www/js/controllers/budget.js
@@ -1,5 +1,45 @@
 angular.module('splitr')
-	.controller('BudgetCtrl', function($scope, budget){
-		$scope.budget = budget;
-	});
+    .controller('BudgetCtrl', function ($scope, $ionicPopup, budget) {
+        $scope.budget = budget;
+        $scope.member = {};
 
+        $scope.addNewMember = function () {
+            // An elaborate, custom popup
+            var myPopup = $ionicPopup.show({
+                templateUrl: 'views/member-popup.html',
+                title: 'New member details',
+                scope: $scope,
+                buttons: [{
+                    text: 'Cancel'
+                }, {
+                    text: '<b>Save</b>',
+                    type: 'button-balanced',
+                    onTap: function (e) {
+                        if (!$scope.member.name) {
+                            //don't allow the user to close unless he enters wifi password
+                            e.preventDefault();
+                        }
+                    }
+                }]
+            });
+            myPopup.then(function () {
+                $scope.budget.members.push($scope.member);
+                $scope.member = {};
+            });
+        };
+
+        $scope.totalForMember = function (member) {
+            var total = 0;
+
+            budget.transactions.forEach(function (tran) {
+                if (tran.payer === member) {
+                    total += tran.amount;
+                }
+                if (tran.participants.indexOf(member) >= 0) {
+                    total -= Math.round(tran.amount / tran.participants.length, 2);
+                }
+            });
+
+            return total;
+        }
+    });
--- www/js/services/fixtures.js
+++ www/js/services/fixtures.js
@@ -12,28 +12,27 @@ angular.module('splitr')
 		var users = [tomek, kuba];
 
 		var budgets = [
-			{ 
-				id: 1, 
-				name: 'Office', 
+			{
+				id: 1,
+				name: 'Office',
 				currency: 'PLN',
-				members: [ 
-					extend(tomek, { total: -120 }),
-					extend(kuba, { total: 550 }),
-					extend(szymon, { total: -50 }),
-					extend(wojtek, { total: -100}),
-					extend(mikro, { total: -40 })
+				members: [
+					tomek,
+					kuba,
+					szymon,
+					wojtek,
+					mikro
 				],
 				transactions: [
-					{ id: 1, date: '2014-09-15', time: '11:30', description: 'Kupno papieru', amount: -20, payer: tomek, participants: [tomek, kuba] },
-					{ id: 2, date: '2014-09-15', time: '13:13', description: 'Drukarka',  amount: -40, payer: kuba, participants: [tomek, kuba]},
-					{ id: 3, date: '2014-09-16', time: '14:11', description: 'Obiad', amount: -60, payer: wojtek, participants: [wojtek, tomek, szymon]},
-					{ id: 4, date: '2014-09-17', time: '14:11', description: 'Obiad', amount: -55, payer: tomek, participants: [tomek, wojtek, mikro]},
-					{ id: 5, date: '2014-09-18', time: '14:11', description: 'Klima', amount: -500, payer: kuba, participants: [tomek, kuba, wojtek, mikro, szymon]},
-					{ id: 6, date: '2014-09-19', time: '14:11', description: 'Klucze', amount: -24, payer: tomek, participants: [tomek, wojtek,kuba,szymon,mikro]},
-					{ id: 7, date: '2014-09-19', time: '14:11', description: 'Sprzątanie', amount: -50, payer: kuba, participants: [tomek, kuba, wojtek, mikro, szymon]},
-					{ id: 8, date: '2014-09-20', time: '14:11', description: 'Obiad', amount: -40, payer: szymon, participants: [tomek, wojtek]}
-				],
-				currentUserTotal: -250 
+					{ id: 1, date: '2014-09-15', time: '11:30', description: 'Kupno papieru', amount: 20, payer: tomek, participants: [tomek, kuba] },
+					{ id: 2, date: '2014-09-15', time: '13:13', description: 'Drukarka',  amount: 40, payer: kuba, participants: [tomek, kuba]},
+					{ id: 3, date: '2014-09-16', time: '14:11', description: 'Obiad', amount: 60, payer: wojtek, participants: [wojtek, tomek, szymon]},
+					{ id: 4, date: '2014-09-17', time: '14:11', description: 'Obiad', amount: 55, payer: tomek, participants: [tomek, wojtek, mikro]},
+					{ id: 5, date: '2014-09-18', time: '14:11', description: 'Klima', amount: 500, payer: kuba, participants: [tomek, kuba, wojtek, mikro, szymon]},
+					{ id: 6, date: '2014-09-19', time: '14:11', description: 'Klucze', amount: 24, payer: tomek, participants: [tomek, wojtek,kuba,szymon,mikro]},
+					{ id: 7, date: '2014-09-19', time: '14:11', description: 'Sprzątanie', amount: 50, payer: kuba, participants: [tomek, kuba, wojtek, mikro, szymon]},
+					{ id: 8, date: '2014-09-20', time: '14:11', description: 'Obiad', amount: 40, payer: szymon, participants: [tomek, wojtek]}
+				]
 			},
 			{ id: 2, name: 'Home', members: [], transactions: [] }
 		];
