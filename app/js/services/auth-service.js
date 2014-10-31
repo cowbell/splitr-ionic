742342542271ebfcc98c28b9e6be9378dd5979af
Tomasz Subik <tsubik@gmail.com>
2014-10-31 13:25:52 +0100

    test infrastructure

--- app/js/services/auth-service.js
+++ app/js/services/auth-service.js
@@ -1,9 +1,18 @@
 angular.module('splitr')
-    .factory('AuthService', function (firebase) {
+    .factory('AuthService', function ($log, firebase) {
         return {
-        	currentUser: null,
-            signIn: function () {
-
+            currentUser: null,
+            signIn: function (user) {
+                var self = this;
+                firebase.authWith(user, function (error, user) {
+                    if (error) {
+                        $log.log(error);
+                    }
+                    if (user) {
+                        $log.log(user);
+                        self.currentUser = user;
+                    }
+                });
             },
             signOut: function () {
 
